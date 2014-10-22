e588f02bcdcae92146249564645847c0eaeb0f9a
Tim Felgentreff <timfelgentreff@gmail.com>
2014-10-22 14:46:29 -0700

    Bugfix for primitive simulation

--- vm.js
+++ vm.js
@@ -3658,7 +3658,7 @@ Object.subclass('Squeak.Primitives',
         // Pop primIndex and argArray, then push args in place...
         this.vm.popN(2);
         for (var i = 0; i < arraySize; i++)
-            this.vm.push(argumentArray[i]);
+            this.vm.push(argumentArray.pointers[i]);
         // Run the primitive
         if (this.doPrimitive(primIdx, arraySize))
             return true;
